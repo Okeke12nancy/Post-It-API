19398ee7cdc28c656c77fca14978a6ae6041303a
Nancy Okeke <ogadimmanancy12@gmail.com>
2023-03-13 09:23:30 -0700

    fix: added comments to the comment schemas

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -4,35 +4,33 @@ import aggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new mongoose.Schema(
   {
+    //post that is being commented on
     post: {
       type: mongoose.Types.ObjectId,
       ref: "Posts",
     },
-
+    // body of the comments
     body: {
       type: String,
       default: "",
     },
-
+    // files to be uploaded
     file: {
       id: {
         type: String,
         default: "",
       },
     },
-
+    // users that commented
     user: {
       type: mongoose.Types.ObjectId,
       ref: "Users",
     },
 
+    // to know whether comments has been deleted or not, default: false
     deleted: {
       type: Boolean,
       default: false,
-    },
-
-    deletedAt: {
-      type: Date,
     },
   },
   { timestamps: true }
